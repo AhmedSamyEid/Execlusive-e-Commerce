97ff6ffd5b9148e04d7cd4e9d2cf0a4f39292fb6
Ahmed Samy <as8609377@gmail.com>
2025-04-08 20:47:07 +0200

    Translation file added

--- src/components/layouts/Banner.tsx
+++ src/components/layouts/Banner.tsx
@@ -1,16 +1,18 @@
+import { useTranslation } from "react-i18next";
 import { Link } from "react-router";
 
-export default function Banner () {
+export default function Banner ({changeLanguage , local}:{changeLanguage:(language :string)=> void ,local:string}) {
+  const { t } =useTranslation();
   return(
     <section className=" bg-black text-white h-[48px]">
       <div className="container m-auto  flex items-center justify-center gap-5 text[14px]">
         <span>
-          Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+            {t("Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!")}
         </span>
-        <Link to={"/"}>ShopNow</Link>
-      <select className="text-white bg-black outline-0" value={"en"}>
+        <Link to={"/"}></Link>
+      <select value={local} onChange={(e) => changeLanguage(e.target.value)} className="text-white bg-black outline-0" >
         <option value={"en"}>English</option>
-        <option value={"en"}>العريبه</option>
+        <option value={"ar"}>العريبه</option>
       </select>
       </div>
     </section>
--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,13 +1,29 @@
 import { Outlet } from "react-router";
 import Banner from "./Banner";
-
+import { useTranslation } from 'react-i18next';
+import { useState } from "react";
 
 
 export default function PageLayout() {
+    const {i18n } = useTranslation();
+  const [local , setLocal] =useState (localStorage.getItem("i18nextLng") || "en"); 
+
+  function changeLanguage (language:string) :void{
+  switch (language){
+    case "ar":
+    i18n.changeLanguage ("ar");
+    setLocal ("ar");
+    break;
+    default:
+    i18n.changeLanguage ("en");
+    setLocal ("en");
+    break;
+  }
+}
   return (
-    <>
-      <Banner />
+    <div dir={local === "ar" ? "rtl" : "ltr"}>
+      <Banner changeLanguage={changeLanguage} local={local} />
       <Outlet />
-    </>
+    </div>
   );
 }
